ac8e80f7b52918a2d0c5238099083f27d477d5c5
NabilKara <karanabil420@gmail.com>
2024-12-03 22:14:24 +0100

    designed the database , added the controllers and modified the dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Nabil',
+            'email' => 'karanabil@gmail.com',
+            'password' => bcrypt('karanabil2004'),
+            'email_verified_at' => now(),
         ]);
+        Project::factory()
+            ->count(30)
+            ->hasTasks(30)
+            ->create();
     }
 }
