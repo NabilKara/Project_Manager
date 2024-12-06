dbec8fba0ca42ea8c86a466ae52782b7ac006a00
NabilKara <karanabil420@gmail.com>
2024-12-06 12:02:59 +0100

    fixed status color issue, removed text wrapping in dates

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
 import {Head, Link, router} from "@inertiajs/react";
 import Pagination from "@/Components/Pagination.jsx";
-import {PROJECT_STATUS_TEXT_MAP} from "@/constants.jsx";
+import {PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP} from "@/constants.jsx";
 import TextInput from "@/Components/TextInput.jsx";
 import SelectInput from "@/Components/SelectInput.jsx";
 
@@ -47,7 +47,7 @@ export default function Index({auth, projects, queryParams=null }){
                                     <th className="px-3 py-3">Status</th>
                                     <th className="px-3 py-3">Create Date</th>
                                     <th className="px-3 py-3 text-nowrap">Due Date</th>
-                                    <th className="px-3 py-3">Created By</th>
+                                    <th className="px-3 py-3 text-nowrap">Created By</th>
                                     <th className="px-3 py-3 text-right">Actions</th>
                                 </tr>
                                 </thead>
@@ -93,13 +93,13 @@ export default function Index({auth, projects, queryParams=null }){
                                         <td className="px-3 py-2">
                                            <span className={
                                                "px-2 py-1 rounded  text-white " +
-                                               PROJECT_STATUS_TEXT_MAP[project.status]
+                                               PROJECT_STATUS_CLASS_MAP[project.status]
                                            }>
                                                 {PROJECT_STATUS_TEXT_MAP[project.status]}
                                            </span>
                                         </td>
-                                        <td className="px-3 py-2">{project.created_at}</td>
-                                        <td className="px-3 py-2">{project.due_date}</td>
+                                        <td className="px-3 py-2 text-nowrap">{project.created_at}</td>
+                                        <td className="px-3 py-2 text-nowrap">{project.due_date}</td>
                                         <td className="px-3 py-2">{project.createdBy.name}</td>
                                         <td className="px-3 py-2">
                                             <Link href={route('project.edit', project.id)}
