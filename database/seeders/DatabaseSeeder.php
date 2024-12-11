579af368abceb9ea29d8e47d4d1fc53f18379267
NabilKara <karanabil420@gmail.com>
2024-12-11 20:26:46 +0100

    added collaborator credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Nabil',
             'email' => 'karanabil@gmail.com',
             'password' => bcrypt('karanabil2004'),
+            'email_verified_at' => now(),
+        ]);
+        User::factory()->create([
+            'name' => 'Amine',
+            'email' => 'aminedana@gmail.com',
+            'password' => bcrypt('aminedana2004'),
             'email_verified_at' => now(),
         ]);
         Project::factory()
